2931968b23b7eb18126b709b40b46d62da63cb6c
ky619 <ky619@naver.com>
2022-07-02 21:30:12 +0900

    sd

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import express from "express";
+
+const app = express();
+const {expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+app.use(expressCspHeader({
+    directives: {
+        'img-src': [SELF, INLINE, "https://image.tmdb.org/t/p/w500"],
+    }
+}));
 
 export default function App({Component, pageProps}){
     return (
